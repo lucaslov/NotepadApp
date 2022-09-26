2a6ac8dac907dc82683365d3fc6f69051a1f7d0d
lucaslov <czyzukasz2402@gmail.com>
2022-09-26 18:08:56 +0200

    Add basic functionality

---
+++ NotepadApp/NotesManager.cs
@@ -0,0 +1,95 @@
+﻿using NoteApp;
+
+namespace NotepadApp
+{
+    public class NotesManager
+    {
+        private List<Note> notes { get; set; }
+
+        public NotesManager()
+        {
+            notes = new List<Note>();
+        }
+
+        public void Add()
+        {
+            var note = GetNoteFromUser();
+            notes.Add(note);
+        }
+
+        private Note GetNoteFromUser()
+        {
+            var note = new Note();
+
+            Console.WriteLine("Podaj notatkę");
+            Console.WriteLine("Podaj Id");
+            note.Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj Description");
+            note.Description = Console.ReadLine();
+            Console.WriteLine("Podaj Name");
+            note.Name = Console.ReadLine();
+            note.CreationDate = DateTime.Now;
+
+            return note;
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Podaj ID notatki do aktualizacji");
+            var id = int.Parse(Console.ReadLine());
+            Note noteToUpdate = null;
+            foreach (var note in notes)
+            {
+                if (note.Id == id)
+                {
+                    noteToUpdate = note;
+                }
+            }
+
+            if (noteToUpdate is null)
+            {
+                Console.WriteLine($"Nie znaleziono notatki z ID: {id}");
+                return;
+            }
+
+            Console.WriteLine("Podaj nowy tytuł");
+            var title = Console.ReadLine();
+
+            Console.WriteLine("Podaj nowy opis");
+            var description = Console.ReadLine();
+
+            noteToUpdate.Description = description;
+            noteToUpdate.Name = title;
+        }
+
+        public void PrintNotes()
+        {
+            foreach (var n in notes)
+            {
+                Console.WriteLine($"Id: {n.Id}, Nazwa: {n.Name}, Opis: {n.Description}");
+            }
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Podaj ID notatki do usunięcia");
+            var id = int.Parse(Console.ReadLine());
+            Note noteToRemove = null;
+            foreach (var note in notes)
+            {
+                if (note.Id == id)
+                {
+                    noteToRemove = note;
+                }
+            }
+
+            if (noteToRemove is null)
+            {
+                Console.WriteLine($"Nie znaleziono notatki z ID: {id}");
+                return;
+            }
+
+            notes.Remove(noteToRemove);
+        }
+    }
+}
--- NotepadApp/Program.cs
+++ NotepadApp/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using NotepadApp;
+
+var noteManager = new NotesManager();
+
+bool isRunning = true;
+
+while (isRunning)
+{
+    Console.WriteLine("Wybierz opcję. 0 - zakończ działanie programu, 1 dodaj notatkę, 2 - usuń notatkę, 3 - edytuj notatkę, 4 - wyświetl wszystkie notatki.");
+
+    int option = int.Parse(Console.ReadLine());
+
+    switch (option)
+    {
+        case 0:
+            isRunning = false;
+            Console.WriteLine("Koniec wykonywania programu.");
+            break;
+        case 1:
+            noteManager.Add();
+            break;
+        case 2:
+            noteManager.Delete();
+            break;
+        case 3:
+            noteManager.Update();
+            break;
+        case 4:
+            noteManager.PrintNotes();
+            break;
+    }
+}
